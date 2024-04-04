c602aa0512f0106205f2efc9bb640f136197e6ae
SAMEEKSHA VARSHNEY <142468768+SameekshaVarshney08@users.noreply.github.com>
2024-04-04 19:59:19 +0530

    Add files via upload

---
+++ DAY3/JS/script.js
@@ -0,0 +1,135 @@
+// console.log('hello');
+// var message="hellooo";
+// var age=18;
+// age="done";
+// console.log(message);
+// function env(){
+//     console.log(message);
+//  var message="hello";
+//  var message="else";
+//  console.log(message);
+// }
+//  for(var i=0;i<4;i++){
+//     console.log('--',i);
+//  }
+//    console.log(i);
+// }
+//  env();
+//  let message="hello";
+//  console.log(message);
+// const messgae="hello";
+// console.log(messgae);
+//  let age=20;
+//  console.log(typeof(age));
+// let useAge='abcd';
+// userAge='abcd';
+// console.log(userAge);
+//  let arr=['a','b','c','d','e','f'];
+//  arr[3]='abcd';
+//  console.log(arr);
+//  let str='12';
+//  let age=20;
+//  let ans=20+1*'12';
+//  console.log(ans);
+// let str='12.12';
+// let p=parseInt(str);
+// let a='12';
+// let b=12;
+// !=
+// !==
+// let a=['a','b','c','d','e','f'];
+// let cpy=[a];
+// console.log(a,cpy);
+// if(a==cpy){
+//     console.log('1.yes');
+// }
+// else{
+//     console.log('1.no');
+// }
+// if(a===cpy){
+//     console.log('2.yes');
+// }
+// else{
+//     console.log('2.no');
+// }
+// const str='sameeksha';
+// const usrName='my name is:';
+// const x=`${usrName} ${str}`;
+// console.log(x);
+//  function  print(str='nothing is here'){
+//    console.log(str);
+//  }
+// //   const s='hello';
+//   print();
+// function sum(a,b){
+//     if(a&&b){
+//     console.log(a+b);
+//     }
+//     else{
+//         console.log(a);
+//     }
+// }
+// sum(10,8);
+// let a='';
+// if(a){
+//  console.log('yes');
+// }
+// else{
+//     console.log('no');
+// }
+// //function declaration
+// function print(){
+//     console.log('--');
+// }
+//  // function assignment(named)
+//  const a=function print(){
+//     console.log('--');
+//  }
+// // function assignment(anonymous)
+// const b=function print(){
+//     console.log('--');
+//  }
+// print();
+// a();
+// b();
+// //arrow function assignment
+// const c=() =>{
+//     console.log('--');
+//  }
+// let a=7;
+// switch(a){
+//     case 1:{
+//     console.log('one');
+//     break;
+//     }
+//     case 2:{
+//         console.log('two');
+//         break;
+//     }
+//     case 3:{
+//         console.log('three');
+//         break;
+//     }
+//     default:
+//         console.log('1,2,3'); 
+// }
+// const obj=new Object();
+// obj[12]='tweleve';
+// obj['name']='tweleve';
+// obj[12]='tweleve';
+// obj[12]='tweleve';
+// console.log(obj);
+// const obj={
+// //     1:'one',
+// //     'name':'sameeksha',
+// //     12:'tweleve',
+// // };
+// //  console.log(obj);
+//  'first name':'sam'
+//  'last name':'sam'
+const obj={
+ 'firstname':'sam',
+ 'lastname':'var',
+};
+const x=prompt('please enter');
+console.log(obj[x]);
